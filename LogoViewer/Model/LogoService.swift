7f7aa577fc2b7f2c0a3d99a25e22586bad9a7a4b
Melanie OBRINGER <caramel2209@outlook.fr>
2019-10-01 18:28:52 +0200

    Add tests

--- LogoViewer/Model/LogoService.swift
+++ LogoViewer/Model/LogoService.swift
@@ -9,14 +9,13 @@
 import Foundation
 
 class LogoService {
-    // create singleton pattern
+    // create singleton patternstatic let shared = LogoService()
     static var shared = LogoService()
-    private init() { }
+    private init() {}
     
-    private var session = URLSession(configuration: .default)
-    
-    init(session: URLSession) {
-        self.session = session
+    private var logoSession = URLSession(configuration: .default)
+    init(logoSession: URLSession) {
+        self.logoSession = logoSession
     }
     
     private var task: URLSessionTask?
@@ -24,25 +23,20 @@ class LogoService {
     
     func getLogo(domain: String, callback: @escaping (Bool, Logo?) -> Void) {
         let completeUrl = "\(logoUrl)\(domain)?size=400"
-        
         let request = URLRequest(url: URL(string: completeUrl)!)
-        
         task?.cancel()
-        task = session.dataTask(with: request, completionHandler: { (data, response, error) in
+        task = logoSession.dataTask(with: request, completionHandler: { (data, response, error) in
             // manage multi-threading with DispatchQueue
             DispatchQueue.main.async {
                 guard let data = data, error == nil else {
                     return callback(false, nil)
                 }
-                
                 guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
                     return callback(false, nil)
                 }
-                
                 callback(true, Logo(imageData: data))
             }
         })
-        
         task?.resume()
     }
 }
